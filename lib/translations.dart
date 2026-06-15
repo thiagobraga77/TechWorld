1f20323a6e421d73577f40e3a786d52a0e79d3c7
Marcos <marcosviniciuscosmo13@gmail.com>
2026-06-15 10:12:18 -0300

    Ah

--- lib/translations.dart
+++ lib/translations.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 
 
 class AppTranslations extends Translations {
